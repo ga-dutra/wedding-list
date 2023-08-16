2af9ccdab9ec7509a0d03e1f51c443be7af7ef08
Gabriel Dutra <99047837+ga-dutra@users.noreply.github.com>
2023-08-16 19:25:46 -0300

    feat: Main Page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Navigate, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<></>} />
+      <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export default function MainPage() {
+  return (
+    <Container>
+      Os shabys
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background-color: #FFCAD4;
+`;
